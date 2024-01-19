5db5a25126f4d01f358f3d8b946850e8d349c9a2
Chengyu Tao <bravolavier@gmail.com>
2024-01-19 10:59:16 -0800

    update tests with predefined test data and convert unittest to pytest

--- tests/test_tidyversetopandas.py
+++ tests/test_tidyversetopandas.py
@@ -1,6 +1,12 @@
 from tidyversetopandas import tidyversetopandas
 import pandas as pd
 import unittest
+import pytest
+
+@pytest.fixture
+def input_df_2():
+    df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": ["a", "b", "c"]})
+    return df
 
 def test_mutate_no_func():
     """Test mutate function without a function"""
@@ -22,36 +28,33 @@ def test_mutate_new_col():
     df = tidyversetopandas.mutate(df, c=lambda x: x["a"] + x["b"])
     assert df["c"].tolist() == [5, 7, 9]
 
-def test_arrange_return_df():
+def test_arrange_return_df(input_df_2):
     """Test the output is a pandas.DataFrame object"""
-    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [0, 1, 9]})
-    df_sorted = tidyversetopandas.arrange(df, True, 'a', 'c')
+    df_sorted = tidyversetopandas.arrange(input_df_2, True, 'A', 'C')
     assert isinstance(df_sorted, pd.DataFrame)
 
-def test_arrange_ascend():
+def test_arrange_ascend(input_df_2):
     """Test the output is in the ascending order"""
-    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [0, 1, 9]})
-    df_sorted = tidyversetopandas.arrange(df, True, 'a', 'c')
+    df_sorted = tidyversetopandas.arrange(input_df_2, True, 'A', 'C')
 
     for i in range(df_sorted.shape[0]-1):
-        assert df_sorted.iloc[i]['a'] <= df_sorted.iloc[i+1]['a']
+        assert df_sorted.iloc[i]['A'] <= df_sorted.iloc[i+1]['A']
 
-def test_arrange_descend():
-    """Test the output is in the ascending order"""
-    """Test the output is in the ascending order"""
-    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [0, 1, 9]})
-    df_sorted = tidyversetopandas.arrange(df, False, 'a', 'c')
+def test_arrange_descend(input_df_2):
+    """Test the output is in the descending order"""
+    df_sorted = tidyversetopandas.arrange(input_df_2, False, 'A', 'C')
 
     for i in range(df_sorted.shape[0]-1):
-        assert df_sorted.iloc[i]['a'] >= df_sorted.iloc[i+1]['a']
+        assert df_sorted.iloc[i]['A'] >= df_sorted.iloc[i+1]['A']
 
-class TestKeyError(unittest.TestCase):
-    def test_column_not_found(self):
-        with self.assertRaises(KeyError):
-            df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [0, 1, 9]})
-            df_sorted = tidyversetopandas.arrange(df, False, 'd', 'e')
-    def test_invalid_column_name(self):
-        with self.assertRaises(TypeError):
-            df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [0, 1, 9]})
-            df_sorted = tidyversetopandas.arrange(df, False, ['a', 'b'], 3)
+def test_arrange_key_error(input_df_2):
+    """Test arrange function returns KeyError when input column name does not exist"""
+    with pytest.raises(KeyError):
+        _ = tidyversetopandas.arrange(input_df_2, False, 'd', 'e')
+
+def test_arrange_type_error(input_df_2):
+    """Test arrange function returns KeyError when input column name does not exist"""
+    with pytest.raises(TypeError):
+        _ = tidyversetopandas.arrange(input_df_2, False, ['a', 'b'], 3)
+
            
